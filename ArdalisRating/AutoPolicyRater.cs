1cd683487a3f088016e224d305aa2f394471e68f
Lawrence Lessa <lawrence.lessa@gmail.com>
2023-10-21 18:16:24 -0300

    Included Flood policy Type maintaining RateEngine

--- ArdalisRating/AutoPolicyRater.cs
+++ ArdalisRating/AutoPolicyRater.cs
@@ -6,17 +6,13 @@ using System.Threading.Tasks;
 
 namespace ArdalisRating
 {
-    public class AutoPolicyRater
+    public class AutoPolicyRater : Rater
     {
-        private readonly RatingEngine _engine;
-        private ConsoleLogger _logger;
 
-        public AutoPolicyRater(RatingEngine engine, ConsoleLogger logger)
+        public AutoPolicyRater(RatingEngine engine, ConsoleLogger logger) : base(engine, logger)
         {
-            _engine = engine;
-            _logger = logger;
         }
-        public void Rate(Policy policy)
+        public override void Rate(Policy policy)
         {
             _logger.Log("Rating AUTO policy...");
             _logger.Log("Validating policy.");
---
+++ ArdalisRating/RaterFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdalisRating
+{
+    public class RaterFactory
+    {
+        public Rater Create(Policy policy, RatingEngine engine)
+        {
+            switch (policy.Type) 
+            {
+                case PolicyType.Auto:
+                    return new AutoPolicyRater(engine, engine.Logger);
+
+                case PolicyType.Flood:
+                    return new FloodPolicyRater(engine,engine.Logger);
+
+                case PolicyType.Land:
+                    return new LandPolicyRater(engine, engine.Logger);
+
+                case PolicyType.Life:
+                    return new LifePolicyRater(engine, engine.Logger);
+
+                default:
+                    // TODO: Implement Null Object Pattern
+                    // Logger.Log("Unkown policy type");
+                    return new UnknownPolicyRater(engine, engine.Logger);
+            }
+        }
+    }
+}
